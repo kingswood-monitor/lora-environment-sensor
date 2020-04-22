a0fca0f0f0193a9746e2369e3c04088fcf9e9441
Richard Lyon <richardlyon@fastmail.com>
2020-04-22 21:05:48 +0100

    create config file

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#include "packet.pb.h"
+
+#define SERVER_ADDRESS 0
+#define CLIENT_ADDRESS 1
+
+#define DEVICE_TYPE DeviceType_EnvironmentSensor
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "util.h"
 #include "radio.h"
 #include "packet.h"
+#include "config.h"
 
 // Packet ID
 uint16_t packet_id = 0;
@@ -24,15 +25,15 @@ void setup()
     Serial.println("Radio started  [OK]");
 
   if (init_sensors())
-  {
     Serial.println("Sensors started [OK]");
-  }
 }
 
 void loop()
 {
-  uint8_t buf[255];
-  uint8_t bytes_written = build_packet(packet_id++, buf, 255);
-  send_packet(packet_id, buf, bytes_written, SERVER_ADDRESS);
+  uint8_t packet[255];
+  uint8_t bytes_written = build_packet(packet_id++, packet, 255);
+
+  send_packet(packet_id, packet, bytes_written, SERVER_ADDRESS);
+
   delay(REFRESH_MILLIS);
 }
