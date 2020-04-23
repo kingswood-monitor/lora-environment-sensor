3bbcc2d4eca99e7be1a1f1bd1e12cc599382dc15
Richard Lyon <richardlyon@fastmail.com>
2020-04-23 10:00:50 +0100

    remove debug print, increase protobuf buffer length

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 #include "packet.pb.h"
 
-#define FIRMWARE_VERSION "1.0.0"
+#define FIRMWARE_VERSION "1.0.1"
 
 // Data refresh rate (milliseconds)
 #define REFRESH_MILLIS 1000 // sensor refresh rate (milliseconds)
