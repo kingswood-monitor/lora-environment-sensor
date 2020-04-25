86176c29376ddf6f3fcb947ddef02507670418d6
Richard Lyon <richardlyon@fastmail.com>
2020-04-26 00:12:55 +0100

    revert to hard coded LOCATION

--- src/config.h
+++ src/config.h
@@ -1,15 +1,7 @@
 #include "packet.pb.h"
 
-/***********************************************************************
- * EEPROM configuration settings                                      */
-
 // Device location
-#define CFG_LOCATION Location_greenhouse
-extern Location location;
-
-// Uncomment the line below write the LOCATION_ID to EEPROM memory
-// #define WRITE_LOCATION_ID_TO_EEPROM
-/*********************************************************************/
+#define CFG_LOCATION Location_outdoor
 
 // Data refresh rate (milliseconds)
 #define CFG_REFRESH_MILLIS 1000 // sensor refresh rate (milliseconds)
