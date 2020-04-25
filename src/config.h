7d6932bf771a00bc09a5f2970dd608f7ee483340
Richard Lyon <richardlyon@fastmail.com>
2020-04-25 15:09:33 +0100

    add feature: get/set location in EEPROM

--- src/config.h
+++ src/config.h
@@ -5,13 +5,17 @@
 
 // Device location
 #define CFG_LOCATION Location_greenhouse
+extern Location location;
+
+// Uncomment the line below write the LOCATION_ID to EEPROM memory
+// #define WRITE_LOCATION_ID_TO_EEPROM
+/*********************************************************************/
+
 // Data refresh rate (milliseconds)
 #define CFG_REFRESH_MILLIS 1000 // sensor refresh rate (milliseconds)
-
-/*********************************************************************/
 
 // Sensor type
 #define SENSOR_TYPE SensorType_environment
 
 // Firmare version
-#define FIRMWARE_VERSION "1.0.2"
+#define FIRMWARE_VERSION "1.0.3"
--- src/util.cpp
+++ src/util.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 
+#include "IdGuard.h"
 #include "config.h"
 #include "util.h"
+
+Location location;
+
+void get_set_config();
 
 bool init_device()
 {
     Serial.begin(115200);
     delay(2000);
+
+    get_set_config();
 
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
@@ -16,4 +23,22 @@ bool init_device()
     Serial.println();
 
     return true;
+}
+
+void get_set_config()
+{
+#ifdef WRITE_LOCATION_ID_TO_EEPROM
+    IdGuard.error_led_pin = LED_BUILTIN;
+    IdGuard.writeIdAndRestartDevice((int)CFG_LOCATION);
+    location = CFG_LOCATION;
+
+    Serial.println(F("INFO: Wrote settings to EEPROM"));
+    Serial.println(F("INFO: Remember to comment WRITE_LOCATION_ID_TO_EEPROM in util.h"));
+#else
+    location = (Location)IdGuard.readId();
+
+    Serial.println(F("INFO: Loaded configuration from EEPROM"));
+#endif
+    Serial.print(F("INFO: LOCATION_ID: "));
+    Serial.println(location);
 }
